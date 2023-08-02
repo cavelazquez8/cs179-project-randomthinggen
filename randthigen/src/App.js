d7ef5ed802118a64cbdc72bc9ff75de484c1aa97
Alison Stewart-Jones <jstew019@ucr.edu>
2023-08-02 16:19:34 -0700

    made initial edits to basic layout

--- randthigen/src/App.js
+++ randthigen/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='title'>
+          RandomThingGen
+        </div>
+        <div className="selectors">
+          <input 
+          type="radio" 
+          name="selector"/>
+          PERSON
+          <input 
+          type="radio" 
+          name="selector"/>
+          PLACE
+          <input 
+          type="radio" 
+          name="selector"/>
+          THING
+        </div>
       </header>
+      <body>
+        <div className='descBox'>
+        Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt 
+        ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, 
+        quis nostrud exercitation 
+        ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in 
+        reprehenderit in voluptate 
+        velit esse cillum dolore 
+        eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat 
+        non proident, sunt in culpa qui 
+        officia deserunt mollit anim id est laborum.
+        </div>
+      </body>
     </div>
   );
 }
