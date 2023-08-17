1ee319121f7f208c05b2e27f158e9dca4e12a6d7
cavelazquez8 <cavelazquez8@gmail.com>
2023-08-17 10:54:13 -0700

    Download Save Results as JSON files

--- randthigen/src/components/SavedResultsContainer.js
+++ randthigen/src/components/SavedResultsContainer.js
@@ -1,9 +1,21 @@
 import styles from "./SavedResultsContainer.module.css";
 const SavedResultsContainer = ({results}) => {
+  const downloadResults = () => {
+    const jsonString = JSON.stringify(results, null, 2);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = url;
+    downloadLink.download = 'saved_results.json';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
   return (
     <div className={styles.savedresultscontainer}>
       <h1 className={styles.savedResults}>Saved Results</h1>
-      <button className={styles.downloadbutton}>
+      <button className={styles.downloadbutton} onClick = {downloadResults}>
         <div className={styles.download}>{`Download `}</div>
         <img
           className={styles.materialSymbolsdownloadIcon}
